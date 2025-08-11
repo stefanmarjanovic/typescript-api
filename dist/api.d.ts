c38bdea8bb3b64a338cf85649e1e5cf113191e03
Stefan Marjanovic <admin@stefanmarjanovic.com>
2025-08-11 18:30:01 +1000

    delete dist directory

--- dist/api.d.ts
+++
@@ -1,7 +0,0 @@
-interface ApiResponse<T> {
-    data: T;
-    status: number;
-}
-export declare function fetchData<T>(url: string): Promise<ApiResponse<T>>;
-export {};
-//# sourceMappingURL=api.d.ts.map
